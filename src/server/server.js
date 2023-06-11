70504ee6c14116dafc8d37c0ac3f4df5e49a75e2
codingLogan <reachCodingLogan@gmail.com>
2023-06-10 20:47:20 -0600

    Add extremely basic csrf tokens

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,20 @@
 import express from 'express'
+import { createToken } from './csrf/csrf.js'
 
 const app = express()
 const port = 3000
 
+// Routes
+app.get('/csrf', (req, res) => {
+  const token = createToken()
+  res.json({ token: token })
+})
+
+app.post('/save', (req, res) => {
+  res.json({ message: 'Saved' })
+})
+
+// Default to serving static files
 app.use(express.static('src/client'))
 
 app.listen(port, () => {
